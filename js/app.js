ac7026162e6c7ab5e32867aeab97ed85b04148e9
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-06 11:28:34 -0500

    spaceman moves!

--- js/app.js
+++ js/app.js
@@ -1,49 +1,3 @@
-
-// Pick a random word from the list
-// - Create a function that selects words from the word list array randomly 
-
-//  Create an empty guessed letter list
-// - Define an empty array for the guessed letters  ( for each ) 
-
-// Set the number of allowed wrong guesses
-// - Create a function that allows the player to to guess with a max of number of attempts 
-
-// Show the word from word list as blank spaces  ( "_ _ _ _")
-// - Create a variable or function to the random word from the word list as blank spaces 
-
-// Repeat players turn until the player wins or runs out of attempts
-// - Create a function that repeats  the players turn until they win or run out of attempts 
-
-//   Display progress
-// - Display the number of wrong guesses left 
-// - Display the letters guessed correctly 
-
-// Ask player to guess a letter
-// - Create a function that asks the player to guess a letter 
-
-//  create a hint 
-
-//  Check if the letter was already guessed
-// - Create a function and if statement that checks if the letter was already guessed
-
-//   Add letter to guessed list
-// - Create a function that removes a letter from the guessed word list 
-
-
-//  Check if the letter is in the secret word
-// - Create a list a function that checks if the letter is a letter of the guessed word 
-// - Create a if statement that display ‘ good guess ‘ if the guessed letter is correct , and display the correct letter in the guessed word
-//     - And if its wrong display “try again’ and decrease the attempts by 1 
-
-//  End the game
-// - Create an end game function 
-//     - If statement to create a displays “ you won’ if the player won 
-// 	if the player loses display you lose and the correct answer 
-// - Create a function to start the game over and a display button to restart the game 
-
-
-
-
 /*-------------------------------- Constants --------------------------------*/
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
     'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -86,7 +40,7 @@ const startGame = () => {
         letterBox.setAttribute('id', `letter-${i}`); // reference MDN > https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
         secretWordContainer.appendChild(letterBox); // reference MDN > https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
     }
-    //document.getElementById('secret-word-display').textContent = secretWordDisplay.join(' ');
+    
 
     letterEls.forEach(button => {
         button.disabled = false;
@@ -111,7 +65,7 @@ const checkGuessedLetter = (event) => {
     let guessedLetter = event.target.innerText;
     lettersGuessed.push(guessedLetter);
     let letterAccess = lettersGuessed[lettersGuessed.length - 1]
-    spaceManImage(remainingChances);
+   
     if ((secretWord.includes(letterAccess))) {
         for (let i = 0; i < secretWord.length; i++) {
             if (secretWord[i] === letterAccess) {
@@ -129,26 +83,28 @@ const checkGuessedLetter = (event) => {
             document.getElementById('restart').classList.remove('hidden')
             disableAllButtons();
         }
-        else if (remainingChances > 0) {
+        
+        else if (remainingChances === 0) {
+         document.getElementById('message').textContent = `Game Over! The word was '${secretWord}'.`;
+            document.getElementById('restart').classList.remove('hidden')
+            disableAllButtons();
+        }
+    } else {
+        if (remainingChances > 0) {
             remainingChances = remainingChances - 1;
+
             if (secretWordDisplay.includes('_')) {
-             spaceManImage(remainingChances)   
+             changeSpaceManImage();   
             }
             document.getElementById('chances').textContent = remainingChances;
 
 
         }
-
-        else if (remainingChances === 0) {
-            document.getElementById('message').textContent = `Game Over! The word was '${secretWord}'.`;
-            document.getElementById('restart').classList.remove('hidden')
-            disableAllButtons();
-        }
     }
     event.target.disabled = true;
 
 };
-const spaceManImage = (remainingChances) => {
+const changeSpaceManImage = () => {
     let imageSelector = document.getElementById('spaceman-image');
     if (remainingChances === 7) {
         imageSelector.src = './images/spaceman-2.png';
@@ -168,7 +124,7 @@ const spaceManImage = (remainingChances) => {
         imageSelector.src = './images/spaceman-9.png';
     }
 };
-
+ 
     // }
     // if (remainingChances === 7) {
     //     imageSelector.src = './images/spaceman-2.png';
@@ -197,7 +153,7 @@ letterEls.forEach(letter => {
 });
 
 letterEls.forEach(letter => {
-    letter.addEventListener('click', spaceManImage)
+    letter.addEventListener('click', changeSpaceManImage)
 });
 
 start.addEventListener('click', startGame);
