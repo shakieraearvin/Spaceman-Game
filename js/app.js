54aa24992a4d2b4fed0c540ee0ce4e8eb5e18368
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-05 23:15:18 -0500

    clean up and image

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ const startBtnEl = document.querySelector('#start');
 const startGame = () => {
     getSecretWord(); // randomly select word from the wordBank array
     secretWordDisplay = [];
-    remainingChances = 9
+    remainingChances = 8
     document.getElementById('chances').textContent = remainingChances;
     document.getElementById('message').textContent = '';
     document.getElementById('restart').classList.add('hidden');
@@ -111,7 +111,7 @@ const checkGuessedLetter = (event) => {
     let guessedLetter = event.target.innerText;
     lettersGuessed.push(guessedLetter);
     let letterAccess = lettersGuessed[lettersGuessed.length - 1]
-
+    spaceManImage(remainingChances);
     if ((secretWord.includes(letterAccess))) {
         for (let i = 0; i < secretWord.length; i++) {
             if (secretWord[i] === letterAccess) {
@@ -123,7 +123,7 @@ const checkGuessedLetter = (event) => {
                 }
             }
         }
-    
+
         if (!secretWordDisplay.includes('_')) {
             document.getElementById('message').textContent = 'You Won 🥳'
             document.getElementById('restart').classList.remove('hidden')
@@ -131,7 +131,12 @@ const checkGuessedLetter = (event) => {
         }
         else if (remainingChances > 0) {
             remainingChances = remainingChances - 1;
+            if (secretWordDisplay.includes('_')) {
+             spaceManImage(remainingChances)   
+            }
             document.getElementById('chances').textContent = remainingChances;
+
+
         }
 
         else if (remainingChances === 0) {
@@ -143,12 +148,56 @@ const checkGuessedLetter = (event) => {
     event.target.disabled = true;
 
 };
+const spaceManImage = (remainingChances) => {
+    let imageSelector = document.getElementById('spaceman-image');
+    if (remainingChances === 7) {
+        imageSelector.src = './images/spaceman-2.png';
+    } else if (remainingChances === 6) {
+        imageSelector.src = './images/spaceman-3.png';
+    } else if (remainingChances === 5) {
+        imageSelector.src = './images/spaceman-4.png';
+    } else if (remainingChances === 4) {
+        imageSelector.src = './images/spaceman-5.png';
+    } else if (remainingChances === 3) {
+        imageSelector.src = './images/spaceman-6.png';
+    } else if (remainingChances === 2) {
+        imageSelector.src = './images/spaceman-7.png';
+    } else if (remainingChances === 1) {
+        imageSelector.src = './images/spaceman-8.png';
+    } else if (remainingChances === 0) {
+        imageSelector.src = './images/spaceman-9.png';
+    }
+};
+
+    // }
+    // if (remainingChances === 7) {
+    //     imageSelector.src = './images/spaceman-2.png';
+    // } else if (remainingChances === 6) {
+    //     imageSelector.src = './images/spaceman-3.png';
+    // } else if (remainingChances === 5) {
+    //     imageSelector.src = './images/spaceman-4.png';
+    // } else if (remainingChances === 4) {
+    //     imageSelector.src = './images/spaceman-5.png';
+    // } else if (remainingChances === 3) {
+    //     imageSelector.src = './images/spaceman-6.png';
+    // } else if (remainingChances === 2) {
+    //     imageSelector.src = './images/spaceman-7.png';
+    // } else if (remainingChances === 1) {
+    //     imageSelector.src = './images/spaceman-8.png';
+    // } else if (remainingChances === 0) {
+    //     imageSelector.src = './images/spaceman-9.png';}
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+
 letterEls.forEach(letter => {
     letter.addEventListener('click', checkGuessedLetter)
+});
+
+letterEls.forEach(letter => {
+    letter.addEventListener('click', spaceManImage)
 });
 
 start.addEventListener('click', startGame);
